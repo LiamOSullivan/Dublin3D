682fa080ebfc35113d6f5b4614c0dca55d6500a2
Liam O'Sullivan <lmosulli@tcd.ie>
2019-07-17 10:21:16 +0100

    Add files via upload

---
+++ Assets/MapData.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+	// Name of the input file, no extension
+	public string inputFile;
+
+	// List for holding data from CSV reader
+	private List<Dictionary<string, object>> zoneList;
+	// The names of cols
+	private List<string> columnList;
+	// Object which will contain instantiated prefabs in hiearchy
+	//public GameObject ZoneHolder;
+
+
+// Use this for initialization
+	void Start ()
+	{
+		// Set list to results of function Reader with argument inputfile
+		zoneList = CSVReader.Read (inputFile);
+
+		// Declare list of strings, fill with keys (column names)
+		columnList = new List<string> (zoneList[1].Keys);
+
+		// Print number of keys (using .count)
+//		Debug.Log ("There are " + columnList.Count + " columns in the CSV");
+//		foreach (string key in columnList) {
+//			Debug.Log ("Column name is " + key);
+//		}	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public List <string> GetZoneInfo (string n_)
+	{
+		//String info = "";
+		//ZoneInfoObject ios = new ZoneInfoObject ("", "");
+		List <string> infoList = new List<string>();
+		//If data is contiguous and ascending...
+		int index;
+		if (Int32.TryParse (n_, out index)) {
+//			Debug.Log ("Returning info for Zone index" + index);
+//			ios = new InfoObject [columnList.Count];
+			for (int i = 0; i < columnList.Count; i += 1) {
+				string name = columnList [i]; //i.e. "object_id"
+				//Debug.Log (" | " + zoneList [index] [name]); //get the object_id entry for each row
+				//info = String.Concat(info, zoneList [index-1][name] + "\t");
+				infoList.Add(""+zoneList [index-1][name]);
+
+			}
+			//Debug.Log (" Info List has size: \n" + infoList.Count);
+			//Debug.Log (" Zone Info: \n" + info);
+			//Debug.Log (" Zone Info Object: " + ios.ObjectID+ "\t : \t"+ios.ZoneOrig);
+			return infoList;
+		} else {
+			//Debug.Log ("MapData | Could not find zone info");
+			infoList.Add("No Info Available");
+			return infoList;
+		}
+	}
+}
---
+++ Assets/MapManager.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapManager : MonoBehaviour
+{
+	Color normalColor = Color.white;
+	Color mouseDownColor = Color.green;
+	Color mouseEnterColor = Color.yellow;
+
+	// Game object which parses and holds data from csv file
+	MapData mdm;
+
+	//"ZoneInfo" GO to display text
+	private GameObject zi;
+
+	// Zone which is currently selected
+	private GameObject zCurr;
+
+	void Start ()
+	{
+		mdm = GetComponentInChildren<MapData> ();
+		zi = GameObject.Find ("ZoneInfo");
+
+	}
+
+	public void mapclick (GameObject objClicked)
+	{
+//		Debug.Log ("Map Manager | Clicked: " + objClicked.name);
+		if(GameObject.Find ("Canvas").transform.childCount>1){
+			int childIndex = 0;
+			foreach (Transform child in GameObject.Find ("Canvas").transform) {
+				//Debug.Log ("Kid:" +childIndex+"\t"+child);
+				if (childIndex > 0) {
+					GameObject.Destroy (child.gameObject);
+				}
+				childIndex += 1;
+			}		
+		}
+
+		MeshRenderer mr = objClicked.GetComponent<MeshRenderer> ();
+		mr.material.color = mouseDownColor;
+		zi.GetComponent<Text> ().text = "";
+		List <string> stringList = new List <string> ();
+		stringList = mdm.GetZoneInfo (objClicked.name);
+		int idx = 0;
+		foreach(string s in stringList){
+			GameObject tClone = Instantiate (zi);//, new Vector3(300, -200+(idx * 75), 0), Quaternion.identity);
+			tClone.transform.SetParent(zi.transform.parent);
+			tClone.transform.position = new Vector3 (900, 700+(idx * -100), 0);
+			Text t = tClone.GetComponent<Text> ();
+			t.enabled = true;
+			t.text = s;
+			idx += 1;
+		}
+		if (zCurr) {
+			mr = zCurr.GetComponent<MeshRenderer> ();
+			mr.material.color = normalColor;
+		}
+		zCurr = objClicked;
+
+	}
+
+	public void mapMouseDown (GameObject objClicked)
+	{
+		//Debug.Log ("Pointer Down: " + objClicked.name);
+
+	}
+	//
+	public void mapMouseUp (GameObject objClicked)
+	{
+		//Debug.Log ("Pointer Up: " + objClicked.name);
+
+//		MeshRenderer mr = objClicked.GetComponent<MeshRenderer> ();
+//		mr.material.color = normalColor;
+//		;
+	}
+
+	public void mapMouseEnter (GameObject objClicked)
+	{
+		//Debug.Log ("Pointer Enter: " + objClicked.name);
+
+		MeshRenderer mr = objClicked.GetComponent<MeshRenderer> ();
+		mr.material.color = mouseEnterColor;
+	}
+
+	public void mapMouseExit (GameObject objClicked)
+	{
+		//Debug.Log ("Pointer Exit: " + objClicked.name);
+		if (objClicked != zCurr) {
+			MeshRenderer mr = objClicked.GetComponent<MeshRenderer> ();
+			mr.material.color = normalColor;
+		}
+	}
+}
