26282d6cbd6e7c46e1edea42976b3adcbe0d609d
Liam O'Sullivan <lmosulli@tcd.ie>
2019-08-28 15:04:10 +0100

    Adapt data structure

---
+++ Assets/Assets/CSVReader.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+  /* This reader takes a flat CSV file with header columns and creates a Dictionary for each row of data. 
+    All of these records are in turn stored in a Dictionary*/
+public class CSVReader
+{
+    static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))"; // Define delimiters, regular expression craziness
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r"; // Define line delimiters, regular experession craziness
+    static char[] TRIM_CHARS = { '\"' };
+
+     
+
+    public static Dictionary<string, Dictionary<string, string>> ReadFile(string file, string key_) //Declare method
+    {
+        Debug.Log("CSVReader is reading " + file); // Print filename, make sure parsed correctly
+
+        /***TODO: Add UI field that allows the keying field to be selected? ***/
+
+        //Put all data in a dictionary keyed by a string (e.g. GUID, OBJECTID or some other index accessible from the shape files)
+        //The object cotaining each record from each row is itself a dictionary
+        var records = new Dictionary<string, Dictionary<string, string>>(); 
+        
+        //Load the file named in the argument of the function as a TextAsset
+        /*** TODO: Is this step necessary? ***/
+        TextAsset data = Resources.Load(file) as TextAsset; 
+        Debug.Log("TextAsset Data loaded:\n" + data); // Print raw data, make sure parsed correctly
+        
+        // Extract the text from the TextAsset
+        var rows = Regex.Split(data.text, LINE_SPLIT_RE); // Split data.text into rows using LINE_SPLIT_RE characters
+        /*** TODO: Set a default object if data is empty or corrupted ***/
+        if (rows.Length <= 1) return records; //Check that there is more than one line
+        string [] headers = Regex.Split(rows[0], SPLIT_RE); //Split header (element 0 from top row of CSV)
+        Debug.Log("headers:\n" + string.Join(", ",headers));
+      
+        // Loop through rows array, add each to outer Dictionary as a record (ignore top row with headers)keyed with the chosen header as index
+        // Within each record add the available values for each variable keyed with the corresponding header
+        for (var i = 1; i < rows.Length; i++)
+
+        {
+            Dictionary<string, string> record = new Dictionary <string, string>();   
+            // Debug.Log("line #"+i+": " + rows[i]);
+            var values = Regex.Split(rows[i], SPLIT_RE); //Split line according to SPLIT_RE, store in var (usually string array)
+            if (values.Length == 0 || values[0] == "") {
+                Debug.Log("Values len ==0 ");
+                continue; // Skip to end of loop if value is 0 length OR first value is empty
+            }
+            
+            if (values.Length != headers.Length) {
+                Debug.Log("Header count and values count mismatch");
+                continue; // Skip if the number of values != no of headers
+            }
+
+            // Loop through every header and add the record to the inner Dictionary for each record
+            for (var j = 0; j < headers.Length; j++)
+            {
+                string key = headers[j];
+                key = key.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", ""); // This is where the headers are parsed
+                
+                string value = values[j]; 
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", ""); // This is where the values are parsed
+                record.Add(key, value);
+                              
+            }
+            
+            //Use the value of OBJECTID as the key for the dictionary
+            //TODO: This assumes OBJECTID is the first column- make  more flexible
+            
+            records.Add(values[0], record);
+            // Debug.Log("Dict entry "+values[0]+"\n"+ZDO);
+                   
+        }
+        return records; 
+    }
+}
---
+++ Assets/Assets/MapData.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+	// Name of the input file (no extension) and key to be used to index the zone data, 
+	public string inputFile;
+	public string inputKey;
+
+	private Dictionary<string, Dictionary<string, string>> records;
+
+	private List<string> keyList;
+	// Object which will contain instantiated prefabs in hiearchy
+	//public GameObject ZoneHolder;
+
+
+// Use this for initialization
+	void Start ()	
+	{	
+		//Check if the Input Key field in the Unity editor has an entry
+		if(String.IsNullOrEmpty(inputKey)){
+			inputKey = "OBJECTID";
+			Debug.Log("Using default key of "+inputKey);
+		}
+		else{			
+			Debug.Log("Using key of "+inputKey);
+		}
+
+		records = CSVReader.ReadFile(inputFile, inputKey);
+		Debug.Log("File "+inputFile +" has records count of "+records.Count);
+		
+		// foreach(){
+
+		// }
+
+		// Declare list of strings, fill with keys (column names)
+		//  keyList = new List<string> (zoneDict[indexConst]); //can index any object in the dictionary to get Keys
+
+		// // Print number of keys (using .Count)
+		// Debug.Log ("There are " + keyList.Count + " columns in the CSV");
+		// foreach (string key in keyList) {
+		// 	Debug.Log ("Key is " + key);
+		// 	Debug.Log("zoneDict: "+zoneDict[0]["OBJECTID"]);
+		// }	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public List <string> GetZoneInfo (string n_)
+	{
+		List <string> infoList = new List<string>();
+		
+		int index;
+		if (Int32.TryParse (n_, out index)) {
+			Debug.Log ("Returning info for Zone " + index);
+//			ios = new InfoObject [keyList.Count];
+			// for (int i = 0; i < keyList.Count; i += 1) {
+			// 	string name = keyList [i]; //i.e. "object_id"
+			// 	//Debug.Log (" | " + zoneDict [index] [name]); //get the object_id entry for each row
+			// 	//info = String.Concat(info, zoneDict [index-1][name] + "\t");
+			// 	infoList.Add(""+zoneDict [index-1][name]);
+
+			// }
+			//Debug.Log (" Info List has size: \n" + infoList.Count);
+			//Debug.Log (" Zone Info: \n" + info);
+			//Debug.Log (" Zone Info Object: " + ios.ObjectID+ "\t : \t"+ios.ZoneOrig);
+			infoList.Add("Info Available");
+			return infoList;
+		} else {
+			//Debug.Log ("MapData | Could not find zone info");
+			infoList.Add("No Info Available");
+			return infoList;
+		}
+	}
+}
